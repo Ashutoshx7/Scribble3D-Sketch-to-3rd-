da174645add68bc910692a934380b92589880633
Karn Ashutosh  Singh <karns7844@gmail.com>
2025-11-10 18:16:10 +0530

    scribble3D

--- Scribble3D/apps/https-backend/src/index.ts
+++ Scribble3D/apps/https-backend/src/index.ts
@@ -1,12 +1,42 @@
-import express from "express";
+import express from "express"
+import {JWT_SECRET }  from "./config"
+import jwt from 'jsonwebtoken';
+import {middleware} from "./middleware"
 
 const app = express();
 const PORT = parseInt(process.env.PORT || "4000", 10);
 
-app.get("/", (_req, res) => {
-	res.send("HTTPS backend is running");
-});
+app.post("signin",(req,res)=>{
+	const userId=1;
 
+	const token=jwt.sign({
+		userId
+	},JWT_SECRET);
+
+	res.json({token})
+
+})
+
+
+
+app.post("/signup",(req,res) =>{
+
+	res.json({userId:"123"})
+
+
+})
+
+
+app.post("/room",middleware,(req,res)=>{
+
+	//db call
+	res.json({
+		roomId:123
+	})
+
+
+
+})
 app.listen(PORT, () => {
 	console.log(`HTTPS backend listening on http://localhost:${PORT}`);
 });
