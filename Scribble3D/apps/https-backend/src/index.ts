0d2ea4b75214372581617d27501be9fd7265cb65
Karn Ashutosh  Singh <karns7844@gmail.com>
2025-11-22 02:01:54 +0530

    zod and schema

--- Scribble3D/apps/https-backend/src/index.ts
+++ Scribble3D/apps/https-backend/src/index.ts
@@ -2,11 +2,31 @@ import express from "express"
 import {JWT_SECRET }  from "./config"
 import jwt from 'jsonwebtoken';
 import {middleware} from "./middleware"
+import {CreateUerSchema,createRoomSchema,SiginSchema}  from "./types";
 
 const app = express();
 const PORT = parseInt(process.env.PORT || "4000", 10);
 
+
+app.post("/signup",(req,res)=>{
+	const data=CreateUerSchema.safeParse(req.body);
+	if(!data.success) {
+		 res.json({
+			message:"incorrect inputs"
+		})
+		return;
+	}
+})
+
 app.post("signin",(req,res)=>{
+
+	const data=SiginSchema.safeParse(req.body);
+	if(!data.success) {
+		 res.json({
+			message:"incorrect inputs"
+		})
+		return;
+	}
 	const userId=1;
 
 	const token=jwt.sign({
@@ -19,15 +39,16 @@ app.post("signin",(req,res)=>{
 
 
 
-app.post("/signup",(req,res) =>{
-
-	res.json({userId:"123"})
-
-
-})
 
 
 app.post("/room",middleware,(req,res)=>{
+	const data=createRoomSchema.safeParse(req.body);
+	if(!data.success) {
+		 res.json({
+			message:"incorrect inputs"
+		})
+		return;
+	}
 
 	//db call
 	res.json({
