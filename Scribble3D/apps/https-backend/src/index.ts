108654289e69e6beab0fd7ea8f64149e5a3a645c
Karn Ashutosh  Singh <karns7844@gmail.com>
2025-12-14 11:00:18 +0530

    fixes

--- Scribble3D/apps/https-backend/src/index.ts
+++ Scribble3D/apps/https-backend/src/index.ts
@@ -3,19 +3,27 @@ import {JWT_SECRET }  from "./config"
 import jwt from 'jsonwebtoken';
 import {middleware} from "./middleware"
 import {CreateUerSchema,createRoomSchema,SiginSchema}  from "./types";
-
+import  { prismaClient } from  "@repo/db/client";
 const app = express();
 const PORT = parseInt(process.env.PORT || "4000", 10);
 
 
 app.post("/signup",(req,res)=>{
-	const data=CreateUerSchema.safeParse(req.body);
-	if(!data.success) {
+	const parsedata=CreateUerSchema.safeParse(req.body);
+	if(!parsedata.success) {
 		 res.json({
 			message:"incorrect inputs"
 		})
 		return;
 	}
+	prismaClient.user.create({
+		data:{
+		email: parsedata.data?.username,
+		password: parsedata.data.password,
+		name:parsedata.data.name
+		}
+
+	})
 })
 
 app.post("signin",(req,res)=>{
